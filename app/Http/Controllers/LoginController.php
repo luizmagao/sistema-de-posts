da50937d58e905b2b9094dc0aa2c0f82434b4917
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-05-14 19:48:30 -0300

    WP: parte de logar por fazer

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -10,5 +11,13 @@ class LoginController extends Controller
     public function login()
     {
         return view('login.login');
+    }
+
+    public function logar(Request $request)
+    {
+        $result = Auth::attempt(['email' => 'test@example.com', 'password' => '12345678']);
+
+        return $result;
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::resource('posts', PostController::class);
 Route::resource('comments', CommentController::class);
 
 Route::get('login', [LoginController::class, 'login'])->name('login');
+Route::get('logar', [LoginController::class, 'logar'])->name('logar');
