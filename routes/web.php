a7acfcfda6b05d6058584b30300f1f22e47a883d
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-05-21 18:37:33 -0300

    WP: redirecinando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return redirect()->route('categories.index', 302);
 });
 
-Route::resource('categories', CategoryController::class);
-Route::resource('posts', PostController::class);
-Route::resource('comments', CommentController::class);
+Route::resource('categories', CategoryController::class)->middleware(Autenticador::class);
+Route::resource('posts', PostController::class)->middleware(Autenticador::class);
+Route::resource('comments', CommentController::class)->middleware(Autenticador::class);
 
 Route::get('login', [LoginController::class, 'login'])->name('login');
 Route::post('logar', [LoginController::class, 'logar'])->name('logar');
